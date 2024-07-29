7649da2fe7edeb5b256bb04ab2e73df36de5af6a
Guillan1000 <souguiga@gmail.com>
2024-07-29 10:24:06 -0300

    Feat: add function list,att and exclude

--- crud.js
+++ crud.js
@@ -3,23 +3,29 @@ const usuarios = [];
 
 let ultimoID = 1;
 
-const cadastro () => {
-    let usuario = [];
+const cadastro = () => {
+    let usuario = {
+        id:'',
+        email:'',
+        nome:'',
+        telefone:[]
+    };
     let entrada;
     while(true) {
         console.log('Digite o e-mail do usuario');
         entrada = prompt();
         let aux = false;
-        usuarios.forEach((user) {
-            if(user.includes(entrada)) {
-                aux = true;
-                break;
-            }
-        });
+        if(usuarios) {
+            usuarios.forEach(function (user) {
+                if(user.email == entrada) {
+                    aux = true;
+                }
+            });
+        }
         if(aux){
             console.log('Erro, e-mail ja cadastrado, digite um e-mail diferente');
         } else {
-            usuario.push(entrada)
+            usuario.email = entrada;
             break;
         }
 
@@ -31,43 +37,91 @@ const cadastro () => {
         if(!entrada) {
             console.log('O nome não pode estar vazio');
         } else {
-            usuario.push(entrada)
+            usuario.nome = entrada;
             break;
         }
         
     }
 
     while(true) {
-        if(usuario.lenght >= 3) {
+        if(usuario.telefone!=0) {
             console.log('Digite um número adicional ou deixe vazio para finalizar o cadastro');
             entrada = +prompt();
             if(!entrada) {
                 break;
             } else {
-                if(entrada < 99999999 || usuario.includes(entrada)) {
+                if(entrada < 99999999 || usuario.entrada) {
                     console.log('O número é invalido ou ja cadastrado');
                 } else {
-                    usuario.push(entrada)
+                    usuario.telefone.push(entrada);
                 }
 
             }
         } else {
             console.log('Digite o telefone do usuario');
             entrada = prompt();
-            if(entrada < 99999999 || usuario.includes(entrada)) {
-                console.log('O número é invalido ou ');
+            if(usuario.telefone.includes(entrada)) {
+                console.log('Telefone ja incluido');
+            } else if(entrada < 99999999) {
+                console.log('O número é invalido');
             } else {
-                usuario.push(entrada)
+                usuario.telefone.push(entrada);
             }
         }
         
     }
 
-    usuario.unshift(ultimoID);
+    usuario.id = ultimoID ;
     ultimoID++;
     usuarios.push(usuario)
     console.log('Usuario cadastrado com sucesso');
 
-       
+};
 
+const listagem = () => {
+    if(usuarios.length == 0){
+        console.log('A Lista esta vazia\n');
+    } else {
+        usuarios.forEach(function (usuario)  {
+            console.log(usuario);
+        });
+    };
+}
+
+const atualizar = () => {
+    if(usuarios.length == 0){
+        console.log('A Lista esta vazia\n');
+    } else {
+        console.log('Digite o ID do usuario que deseja atualizar');
+        listagem();
+        let entrada = prompt();
+        usuarios.forEach(function (usuario) {
+            if(entrada == usuario.id) {
+                cadastro();
+            }
+        })
+    }
+}
+
+const excluir = () => {
+    if(usuarios.length == 0){
+        console.log('A Lista esta vazia\n');
+    } else {
+        console.log('Digite o ID do usuario que deseja excluir');
+        listagem();
+        let entrada = prompt();
+        usuarios.forEach(function (usuario, index) {
+            if(entrada == usuario.id) {
+                usuarios.splice(index,1)
+                console.log('Usuario exlcuido');
+            } 
+        });
+    }
+}
+
+module.exports = {
+    cadastro,
+    listagem,
+    atualizar,
+    excluir
 }
--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-const promp = require('prompt-sync')();
+const prompt = require('prompt-sync')();
+const crud = require('./crud')
+
+while(true) {
+    console.log('Digite:\n1.Cadastro\n2.Listagem\n3.Atualizar\n4.Excluir\n5.Sair\n');
+    let ent = prompt()
+    switch (ent) {
+        case '1':
+            crud.cadastro();
+            break;
+        case '2':
+            crud.listagem();
+            break;
+        case '3':
+            crud.atualizar();
+            break;
+        case '4':
+            crud.excluir();
+            break;
+        case '5':
+            process.exit();
+        default:
+            console.log('Entrada invalida\n');
+            break;
+    }
+}
+
+
